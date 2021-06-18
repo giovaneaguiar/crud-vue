a936704bed7c430c16d32c913897e3948cfe40ca
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2021-06-18 12:39:12 -0300

    cria formulário com b-card.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <template>
   <div id="app">
-    <h1>Formulário Giovane Aguiar</h1>
+    <h1>Formulário</h1>
+    <b-card>
+      <b-form-group label="Nome: ">
+        <b-form-input
+          type="text"
+          size="lg"
+          v-model="usuario.nome"
+          placeholder="Informe o Nome"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group label="E-mail: ">
+        <b-form-input
+          type="email"
+          size="lg"
+          v-model="usuario.email"
+          placeholder="Informe o E-mail"
+        ></b-form-input>
+      </b-form-group>
+      <hr>
+      <b-button @click.prevent="salvar" size="lg" variant="primary">Salvar</b-button>
+    </b-card>
   </div>
 </template>
 
@@ -8,6 +28,15 @@
 
 
 export default {
+
+  data(){
+    return {
+      usuario: {
+        nome: '',
+        email: '',
+      }
+    }
+  }
 
 }
 </script>
