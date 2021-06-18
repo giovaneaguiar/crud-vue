e715bf363d99f39dbbd647587b57d0508367300a
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2021-06-18 12:28:25 -0300

    primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Formlário Giovane Aguiar</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
