7e7ce2cb45f63e28be7a6463630b58b1af6ddf94
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2021-06-19 13:13:45 -0300

    implementa assunto sobre headers.

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,7 +4,13 @@ import axios from "axios";
 Vue.use({
   install(Vue) {
     Vue.prototype.$http = axios.create({
-      baseURL: "https://formulario-giovane-default-rtdb.firebaseio.com/"
+      baseURL: "https://formulario-giovane-default-rtdb.firebaseio.com/",
+      headers: {
+        //header global.
+        Authorization: "abc123"
+        //qualquer requisição que acontecer, o authorization
+        //estará presente.
+      }
     });
   }
 });
